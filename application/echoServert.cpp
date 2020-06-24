fdc6790a97fb8db33cb3759e7231e743b3d07876
fwei <fuweiagn@outlook.com>
2020-06-24 15:28:37 +0800

    echoServert: Changed to a prethreading based event-driven echo server.

--- application/echoServert.cpp
+++ application/echoServert.cpp
@@ -2,7 +2,7 @@
  * File: echoServert.cpp
  * ---------------------
  * @author: Fu Wei
- * simple multi-thread echo server demo.
+ * Simple prethreading based event-driven echo server demo.
  * Just for linux, so Posix thread is Ok.
  */
 
@@ -12,37 +12,22 @@
 #include "io/robustio.hpp"
 #include "io/sio.hpp"
 #include <iostream>
+#include <mutex>
+#include "sem/sharedBuf.hpp"
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "EndlessLoop"
 using namespace std;
 
 const int MAXLINE = 8192;
+const int NTHREADS = 10;
+SharedBuf buf(10);
 
-void echo(int connfd)
-{
-    size_t n;
-    char buf[MAXLINE];
-    Riob riob(connfd);
+size_t byte_cnt = 0;
+mutex mtx;
 
-    while ((n = riob.readlineb(buf, MAXLINE)) != 0)
-    {
-        cout << "server received " << n << " bytes\n";
-        Rio::writen(connfd, buf, n);
-    }
-}
-
-void *echoThread(void *argp) {
-    int connfd = *(int *)argp;
-    Pthread_detach(Pthread_self());
-    delete (int*)argp; // we need to free the memory of connfd pointer
-
-    echo(connfd);
-    Close(connfd);
-
-    cout << "thread " << Pthread_self() << " finished\n";
-    return nullptr;
-}
+void echo(int connfd);
+void* echoThread(void* argp);
 
 int main(int argc, char* argv[])
 {
@@ -54,18 +39,53 @@ int main(int argc, char* argv[])
 
     char* port = argv[1];
 
-    char client_hostname[MAXLINE], client_port[MAXLINE];
     struct sockaddr clientaddr;
 
     int listenfd = OpenListenFd(port);
 
-    while (true) {
-        socklen_t clientlen = sizeof(struct sockaddr);
-        int *connfdp = new int;
-        *connfdp = Accept(listenfd, &clientaddr, &clientlen);
+    // create worker threads
+    for (int i = 0; i < NTHREADS; i++)
+    {
         pthread_t pid;
-        Pthread_create(&pid, nullptr, echoThread, connfdp);
+        Pthread_create(&pid, nullptr, echoThread, nullptr);
     }
+
+    while (true)
+    {
+        socklen_t clientlen = sizeof(struct sockaddr);
+        int connfd = Accept(listenfd, &clientaddr, &clientlen);
+        buf.insert(connfd);
+    }
+}
+
+void echo(int connfd)
+{
+    size_t n;
+    char buf[MAXLINE];
+    Riob riob(connfd);
+
+    while ((n = riob.readlineb(buf, MAXLINE)) != 0)
+    {
+        unique_lock<mutex> lock(mtx);
+        byte_cnt += n;
+        cout << "server received " << n << " bytes (total: " << byte_cnt << " bytes)\n";
+        lock.unlock();
+        Rio::writen(connfd, buf, n);
+    }
+}
+
+void* echoThread(void* argp)
+{
+    Pthread_detach(Pthread_self());
+
+    while (true)
+    {
+        int connfd = buf.remove();
+        echo(connfd);
+        Close(connfd);
+    }
+
+    return nullptr;
 }
 
 #pragma clang diagnostic pop
