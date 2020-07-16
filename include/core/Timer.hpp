4aebad2a4b21fa3dc837ed632782c936ca3cd121
fwei <fuweiagn@outlook.com>
2020-07-16 22:04:25 +0800

    add repeat feature for Timer

--- include/core/Timer.hpp
+++ include/core/Timer.hpp
@@ -5,7 +5,6 @@
  * Timer is a basic component of timer classes family. Each Timer has its own call back function
  * and expiration time. User should not use Timer directly but TimerQueue instead. TimerQueue
  * do the management job of Timers while EventLoop adds them to a TimerQueue.
- * // TODO: repeat feature
  */
 
 #ifndef HYDROGENWEB_TIMER_HPP
@@ -21,13 +20,15 @@ class Timer
 {
 public:
     Timer(const TimerCallBack& cb, TimeStamp endTime, double interval) :
-        _cb(cb), _endTime(endTime), _interval(interval)
+        _cb(cb), _endTime(endTime), _interval(interval), _repeat(interval > 0.0)
     {
     }
 
     void run() const { _cb(); };
 
     TimeStamp expiredTime() const { return _endTime; }
+    bool repeat() const { return _repeat; }
+    void restart(TimeStamp now);
 
     /*
      * make it non-copyable
@@ -36,8 +37,9 @@ public:
     Timer& operator=(const Timer&) = delete;
 
 private:
-    TimerCallBack _cb;
+    const TimerCallBack _cb;
     TimeStamp _endTime; // expiration time
-    double _interval;
+    const double _interval;
+    const bool _repeat; // true if _interval > 0.0
 };
 #endif //HYDROGENWEB_TIMER_HPP
--- src/Timer.cpp
+++ src/Timer.cpp
@@ -6,3 +6,11 @@
  */
 
 #include "core/Timer.hpp"
+
+void Timer::restart(TimeStamp now)
+{
+    if (_repeat)
+    {
+        _endTime = now + _interval;
+    }
+}
