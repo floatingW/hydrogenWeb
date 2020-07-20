0dc6ccb8c9df18e67d640e4b8761ce2fff1ec76d
fwei <fuweiagn@outlook.com>
2020-07-20 20:44:28 +0800

    add tests for TcpServer/TcpConnection

---
+++ tests/TcpServer_test.cpp
@@ -0,0 +1,54 @@
+/*
+ * File: TcpServer_test.cpp
+ * ------------------------
+ * @author: Fu Wei
+ *
+ */
+
+#include "network/EventLoop.hpp"
+#include "network/TcpConnection.hpp"
+#include "network/TcpServer.hpp"
+#include "network/InetAddr.hpp"
+
+#include <unistd.h>
+
+typedef std::shared_ptr<TcpConnection> pTcpConnection;
+
+void onConnection(const pTcpConnection& conn)
+{
+    if (conn->connected())
+    {
+        printf("onConnection(): new connection [%s] from %s\n",
+               conn->name().c_str(),
+               conn->clientAddr().toString().c_str());
+    }
+    else
+    {
+        printf("onConnection(): connection [%s] is down\n",
+               conn->name().c_str());
+    }
+}
+
+void onMessage(const pTcpConnection& conn,
+               const char* data,
+               ssize_t len)
+{
+    printf("onMessage(): received %zd bytes from connection [%s]\n",
+           len,
+           conn->name().c_str());
+}
+
+int main(int argc, char* argv[])
+{
+    printf("in main(): pid = %d\n", getpid());
+
+    InetAddr listenAddr(23456);
+    EventLoop loop;
+    TcpServer server(&loop, listenAddr);
+    server.setConnectionCallback(onConnection);
+    server.setMessageCallback(onMessage);
+    server.run();
+    loop.loop();
+
+    return 0;
+}
